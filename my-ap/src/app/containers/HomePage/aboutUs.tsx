e6479c762dab0b294144dc17d935d40d58765e36
Tim Davidson <timdavidson2@gmail.com>
2021-11-13 01:56:58 -0500

    update about us page with image and text

--- my-ap/src/app/containers/HomePage/aboutUs.tsx
+++ my-ap/src/app/containers/HomePage/aboutUs.tsx
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 import tw from 'twin.macro';
 import { SCREENS } from '../../components/responsive';
 
+import LamboImg from '../../../assets/images/lambo.png';
+
 const AboutUsContainer = styled.div`
   ${tw`
     w-full
@@ -19,3 +21,78 @@ const AboutUsContainer = styled.div`
     bg-white
   `};
 `;
+
+const CarContainer = styled.div`
+  width: auto;
+  height: 15em;
+  margin-left: -50px;
+  img {
+    width: auto;
+    height: 100%;
+  }
+  @media (min-width: ${SCREENS.md}) {
+    height: 14rem;
+  }
+  @media (min-width: ${SCREENS.lg}) {
+    height: 15rem;
+  }
+  @media (min-width: ${SCREENS['2xl']}) {
+    height: 17.5rem;
+    margin-left: 0;
+  }
+`;
+
+const InfoContainer = styled.div`
+  ${tw`
+    md:w-1/2
+    flex
+    flex-col
+    md:ml-6
+    2xl:ml-16
+  `};
+`;
+
+const Title = styled.h1`
+  ${tw`
+    text-black
+    text-2xl
+    md:text-5xl
+    font-extrabold
+    md:font-black
+    md:leading-normal
+  `};
+`;
+
+const InfoText = styled.p`
+  ${tw`
+    md:max-w-2xl
+    text-sm
+    md:text-base
+    text-gray-500
+    font-normal
+    mt-4
+  `};
+`;
+
+export function AboutUs() {
+  return (
+    <AboutUsContainer>
+      <CarContainer>
+        <img src={LamboImg} />
+      </CarContainer>
+      <InfoContainer>
+        <Title>
+          Come Make Your Dreams Come True, Once In A Lifetime Experience
+        </Title>
+        <InfoText>
+          We know the difference is in the details and that’s why our exotic and
+          luxury car rental services, in the tourism and business industry,
+          stand out for their quality and good taste, to offer you an unique
+          experience. Choose from our selection of high-end, late model, exotic
+          sports cars, sedans and SUVs from some of the world's top luxury car
+          manufacturers.
+        </InfoText>
+      </InfoContainer>
+    </AboutUsContainer>
+  );
+}
--- my-ap/src/app/containers/HomePage/index.tsx
+++ my-ap/src/app/containers/HomePage/index.tsx
@@ -4,6 +4,7 @@ import tw from 'twin.macro';
 import { BookCard } from '../../components/bookCard';
 import { Marginer } from '../../components/marginer';
 import { NavBar } from '../../components/navbar';
+import { AboutUs } from './aboutUs';
 import { BookingSteps } from './bookingSteps';
 import { TopSection } from './topSection';
 
@@ -27,6 +28,8 @@ export function HomePage() {
       <BookCard />
       <Marginer direction="vertical" margin="10rem" />
       <BookingSteps />
+      <Marginer direction="vertical" margin="5rem" />
+      <AboutUs />
     </PageContainer>
   );
 }
