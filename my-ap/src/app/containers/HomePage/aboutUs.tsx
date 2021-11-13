c9e1e8a3764ddfa4a1d15d3fbc8f9fd55b5dde0d
Tim Davidson <timdavidson2@gmail.com>
2021-11-13 01:29:53 -0500

    add aboutUs page and start to style

---
+++ my-ap/src/app/containers/HomePage/aboutUs.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+import tw from 'twin.macro';
+import { SCREENS } from '../../components/responsive';
+
+const AboutUsContainer = styled.div`
+  ${tw`
+    w-full
+    flex
+    flex-wrap
+    items-center
+    justify-center
+    pt-4
+    pb-4
+    pr-7
+    pl-7
+    md:pl-0
+    md:pr-0
+    bg-white
+  `};
+`;
