75dcc34a16b11497f985e3c947ab33ed0fc1fb1d
Tim Davidson <timdavidson2@gmail.com>
2021-11-13 08:28:04 -0500

    add generate-typings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {ConfigModule} from 'nest-config'
+import { MongooseModule } from '@nestjs/mongoose';
+import { GraphQLModule } from '@nestjs/graphql';
+import { GraphQLDefinitionsFactory } from '@nestjs/graphql';
+// import { join } from 'path';
 
+const URI = process.env.MONGODB_URL;
+// const definitionsFactory = new GraphQLDefinitionsFactory();
 @Module({
-  imports: [ConfigModule.forRoot()],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot( 'mongodb://mongodb:27017/nestjs', {
+      connectionFactory: (connection) => {
+        connection.plugin(require('mongoose-autopopulate'));
+        return connection;
+      }
+    }),
+    GraphQLModule.forRoot({ 
+      playground: (process.env.NODE_ENV === "development" && true) || false,
+      debug: (process.env.NODE_ENV === "development" && true) || false,
+      installSubscriptionHandlers: true,
+      autoSchemaFile:true,
+}),
+],
+    controllers: [AppController],   
+    providers: [AppService],
 })
 export class AppModule {}
+
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,13 +1,24 @@
-import { Module} from '@nestjs/common';
-import {Connection} from "typeorm"
-import {TypeOrmModule} from "@nestjs/typeorm"
+import { Module } from '@nestjs/common';
+import { MongoClient, Db } from 'mongodb';
 
+const URI = process.env.MONGODB_URL;
 @Module({
-  imports: [TypeOrmModule.forRoot()],
-  exports: [TypeOrmModule]
+  providers:[
+  {
+      provide: 'DATABASE_CONNECTION',
+      useFactory: async (): Promise<Db> => {
+        try {
+          const client = await MongoClient.connect( 'mongodb://mongodb:27017/nestjs', { });
+            return client.db(URI);
+        } catch (e) {
+          throw e;
+        }
+      }
+    },
+  ],
+  exports: ['DATABASE_CONNECTION'],
 })
+export class DatabaseModule {}
 
-export class DatabaseModule {
-  constructor(connection: Connection)
-}
+
 
