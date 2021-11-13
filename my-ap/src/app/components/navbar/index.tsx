30ba804b73f358d7a6485afc99f775ab7f9795f9
Tim Davidson <timdavidson2@gmail.com>
2021-11-12 20:00:43 -0500

    add burger  menu to nav

--- my-ap/src/app/components/navbar/index.tsx
+++ my-ap/src/app/components/navbar/index.tsx
@@ -1,20 +1,11 @@
-import React from 'react';
 import styled from 'styled-components';
 import tw from 'twin.macro';
 import { Logo } from '../logo';
 import { NavItems } from './navitems';
 
 const NavbarContainer = styled.div`
-  min-heght: 68px;
-  ${tw`
-    w-full
-    max-w-2xl
-    flex
-    items-center
-    lg:pl-12 
-    lg:pr-12 
-    justify-between
-`}
+  min-heght: 4.375rem;
+  ${tw`flex items-center justify-between w-full max-w-screen-2xl lg:pl-12 lg:pr-12`}
 `;
 
 const LogoContainer = styled.div``;
--- my-ap/src/app/components/navbar/navitems.tsx
+++ my-ap/src/app/components/navbar/navitems.tsx
@@ -1,34 +1,41 @@
 import React from 'react';
 import styled from 'styled-components';
 import tw from 'twin.macro';
-import { NavLink } from 'react-router-dom';
+import { Slide as Menu } from 'react-burger-menu';
+import { useMediaQuery } from 'react-responsive';
+import { SCREENS } from '../responsive/index';
 
 const ListContainer = styled.ul`
-  ${tw`
-    flex
-    list-none
-  `};
+  ${tw`flex list-none `};
 `;
 
 const NavItem = styled.li`
-  ${tw`
-    text-xs
-    md:text-base
-    text-black
-    font-medium
-    mr-5 
-    md:mr-5 
-    cursor-pointer
-    transition
-    duration-500 
-    ease-in-out 
-    
-
-    
-`}
+  ${tw`mr-5 text-xs font-medium text-black transition duration-500 ease-in-out cursor-pointer md:text-base md:mr-5`}
 `;
 
 export function NavItems() {
+  const isMobile = useMediaQuery({ maxWidth: SCREENS.sm });
+
+  if (isMobile)
+    return (
+      <Menu>
+        <ListContainer>
+          <NavItem>
+            <a href="#">Home</a>
+          </NavItem>
+          <NavItem>
+            <a href="#">Cars</a>
+          </NavItem>
+          <NavItem>
+            <a href="#">Services</a>
+          </NavItem>
+          <NavItem>
+            <a href="#">Contact Us</a>
+          </NavItem>
+        </ListContainer>
+      </Menu>
+    );
+
   return (
     <ListContainer>
       <NavItem>
