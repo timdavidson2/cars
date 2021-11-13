e50de501cc7b06a7abb247a9caaa3c615320a94c
Tim Davidson <timdavidson2@gmail.com>
2021-11-13 01:00:25 -0500

    add booking instructions file

---
+++ my-ap/src/app/containers/HomePage/bookingSteps.tsx
@@ -0,0 +1,9 @@
+import {
+  faCalendarAlt,
+  faCarSide,
+  faMapMarkedAlt,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import styled from 'styled-components';
+import tw from 'twin.macro';
