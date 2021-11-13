487908f79b5f0100dba8232dfd3a03469476c242
Tim Davidson <timdavidson2@gmail.com>
2021-11-13 01:01:33 -0500

    add container for booking

--- my-ap/src/app/containers/HomePage/bookingSteps.tsx
+++ my-ap/src/app/containers/HomePage/bookingSteps.tsx
@@ -7,3 +7,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import styled from 'styled-components';
 import tw from 'twin.macro';
+
+const Container = styled.div`
+  ${tw`
+    w-full
+    flex
+    flex-col
+    items-center
+    pt-3
+    pb-3
+    lg:pt-6
+    lg:pb-6
+  `};
+`;
